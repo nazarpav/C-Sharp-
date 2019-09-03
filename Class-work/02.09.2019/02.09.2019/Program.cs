867095e4ec48eba7893e6f82d0ad613a1d27b89e
павлюк назар <hasab.k18@gmail.com>
2019-09-03 18:40:57 +0300

    first_push 03.09.2019

---
+++ Class-work/02.09.2019/02.09.2019/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._09._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int x=8;
+            Console.Write($" { x }\n\n\n\tg\t\tg");
+            
+        }
+    }
+}
---
+++ Class-work/03.09.2019/03.09.2019/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._09._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine();
+            for(int i=0;i<10;i++)
+            {
+                Console.Write("  ");
+                for (int j = 0; j < 20; j++)
+                {
+                    Console.Write('▓');
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
