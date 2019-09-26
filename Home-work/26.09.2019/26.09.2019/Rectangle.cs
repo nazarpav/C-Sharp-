0b6def85861081a2a0c9359d2d2e25ea1a5f4c46
павлюк назар <hasab.k18@gmail.com>
2019-09-27 00:07:28 +0300

    26.09.2019

--- Home-work/26.09.2019/26.09.2019/Rectangle.cs
+++ Home-work/26.09.2019/26.09.2019/Rectangle.cs
@@ -7,7 +7,11 @@ namespace _26._09._2019
     class Rectangle
     {
         private int A, B;
-
+        public Rectangle(int A,int B)
+        {
+            this.A = A;
+            this.B = B;
+        }
         public int A1 { get => A; set => A = value; }
         public int B1 { get => B; set => B = value; }
         public override string ToString() => ($"A: {A} | B: {B}");
@@ -16,7 +20,8 @@ namespace _26._09._2019
         {
             Rectangle rect = (Rectangle)obj;
             return A == rect?.A && B == rect?.B;
-        }​
+        }
+
         public override int GetHashCode()
         {
             var hashCode = 1861411795;
@@ -24,5 +29,25 @@ namespace _26._09._2019
             hashCode = hashCode * -1521134295 + B.GetHashCode();
             return hashCode;
         }
+        public static Rectangle operator ++(Rectangle rect)
+        {
+            rect.A++;
+            rect.B++;
+            return rect;
+        }
+        public static Rectangle operator --(Rectangle rect)
+        {
+            rect.A--;
+            rect.B--;
+            return rect;
+        }
+        public static Rectangle operator -(Rectangle rect) => new Rectangle(rect.A * -1, rect.B * -1);
+        public static Rectangle operator +(Rectangle rect, Rectangle rect2) => new Rectangle(rect.A + rect2.A, rect.B + rect2.B);
+        public static Rectangle operator -(Rectangle rect, Rectangle rect2) => new Rectangle(rect.A - rect2.A, rect.B - rect2.B);
+        public static Rectangle operator *(Rectangle rect, Rectangle rect2) => new Rectangle(rect.A * rect2.A, rect.B * rect2.B);
+        public static Rectangle operator /(Rectangle rect, Rectangle rect2) => new Rectangle(rect.A / rect2.A, rect.B / rect2.B);
+        public static bool operator >(Rectangle rect, Rectangle rect2) => rect.A + rect.B > rect2.A + rect2.B;
+        public static bool operator <(Rectangle rect, Rectangle rect2) => !(rect>rect2);
+
     }
 }
