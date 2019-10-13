fa4a009f6284e2f332dde0a5075835807c62f427
павлюк назар <hasab.k18@gmail.com>
2019-10-13 23:41:44 +0300

    13.10.2019

---
+++ Home-work/13.10.2019/13.10.2019/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _13._10._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regular regex = new Regular();
+            regex.Task1();
+        }
+    }
+}
---
+++ Home-work/13.10.2019/13.10.2019/Regular.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _13._10._2019
+{
+    class Regular
+    {
+        public void Task1()
+        {
+            string str = System.IO.File.ReadAllText("Task1.txt");
+            Regex regex = new Regex(@"\S*\w*\S*");
+            MatchCollection matches = regex.Matches(str);
+            str = "";
+            foreach (Match i in matches)
+            {
+                if (string.IsNullOrWhiteSpace(i.Value)) continue;
+                str += i.Value.ToCharArray()[0] = i.Value.ToString().ToUpper()[0] ;
+                str += '.';
+            }
+            System.IO.File.WriteAllText("Task1.txt", str);
+        }
+    }
+}
